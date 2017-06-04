90e1d0e3b24600f0b5eb95ec6b35cb48b716d618
Pedro Nunes <preisnunes@gmail.com>
2017-06-04 17:36:22 +0100

    update gitignore

---
+++ db/migrations/20170604163244_create_ip_country_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateIpCountryTable extends AbstractMigration
+{
+
+    protected $tableName = 'ipcountry';
+
+    public function up()
+    {
+
+        if( $this->hasTable($this->tableName) ) {
+            return;
+        }
+
+        $createSql = "CREATE TABLE IF NOT EXISTS `$this->tableName` (
+                  `id` INT(1) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, -- the id just for numeric
+                  `ip_start`   VARCHAR(50) COLLATE UTF8_GENERAL_CI NOT NULL, -- the ip start from maxmind data
+                  `ip_end`     VARCHAR(50) COLLATE UTF8_GENERAL_CI NOT NULL, -- the ip end of maxmind data
+                  `longitude_id`   INT(1) UNSIGNED ZEROFILL NOT NULL, -- the start of maxmind longitude id 
+                  `latitude_id`     INT(1) UNSIGNED ZEROFILL NOT NULL, -- the end of maxmind latitude id
+                  `country_code`  VARCHAR(4) COLLATE UTF8_GENERAL_CI NOT NULL, -- the country code
+                  `country`   VARCHAR(100) COLLATE UTF8_GENERAL_CI NOT NULL, -- the country name
+                  PRIMARY KEY( `id`,`ip_start`,`ip_end`)
+                ) DEFAULT CHARSET=UTF8 COLLATE=UTF8_GENERAL_CI AUTO_INCREMENT=1 ;";
+
+        $this->execute( $createSql );
+
+    }
+
+    public function down(){
+
+        if( !$this->hasTable($this->tableName) ) {
+            return;
+        }
+
+        $this->execute( "DROP table $this->tableName" );
+
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,23 @@
+<?php
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../vendor/autoload.php';
+
+$app = new \Slim\App;
+$app->get('/locationByIP/', function (Request $request, Response $response) {
+
+    $response->getBody()->write("Hello, World");
+    return $response;
+
+});
+
+$app->get('/', function (Request $request, Response $response) {
+
+    $response->getBody()->write("Hello, World");
+    return $response;
+
+});
+
+$app->run();
